3af46e74a54076d56658c989d3c5f8596b512c6b
Саша Руцман <rutsman@bk.ru>
2019-08-27 14:50:11 +0300

    initial commit

---
+++ MVVM Practice/Profile.swift
@@ -0,0 +1,15 @@
+//
+//  Profile.swift
+//  MVVM Practice
+//
+//  Created by Саша Руцман on 27.08.2019.
+//  Copyright © 2019 Саша Руцман. All rights reserved.
+//
+
+import Foundation
+
+struct Profile {
+    var name: String
+    var secondName: String
+    var age: Int
+}
---
+++ MVVM Practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVM Practice
+//
+//  Created by Саша Руцман on 27.08.2019.
+//  Copyright © 2019 Саша Руцман. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
